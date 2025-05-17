248998fd65510db3b59fe97ef536676a7b615dd8
XGhostD <122043762@upq.edu.mx>
2025-05-16 23:46:09 -0600

    Update Functions.js

---
+++ Functions.js
@@ -0,0 +1,19 @@
+var nombre = "Armando";
+var edad = 25;
+
+nombre = "Ana Maria";
+
+var saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+//muestra en consola en contenido de saludo
+ function cuadrado(numero) {
+    return numero * numero;
+ }
+
+ //console.log();
+ //muestra en consola en probando con 3 numero diferentes
+
+ /* Crea un arrow function llamada saludoPersonalizado que reciba dos
+    parametros: nombre y edad, y retorne una cadena como la siguiente */
+
+//"Hola, me llamo Isay y tengo 37 años"
