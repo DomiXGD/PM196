f2501d3f70e4152444bca0ff1931b8054594e1b0
XGhostD <122043762@upq.edu.mx>
2025-05-20 18:02:35 -0600

    Ejercicio A
    Hice algunas modificaciones ya que no vi bien el objeto

--- EjercicioA.js
+++ EjercicioA.js
@@ -1,10 +1,13 @@
 //Tienes el siguiente objeto persona, Exrtrae los valores de nombre, edad y cuidad usando destructuracion y luego muestra un mensaje como "Me llamo Domingo Araujo, tengo 37 años y vivo en Queretaro"
 const persona = {
     nombre: "Domingo Araujo",
-    edad: 37,
-    ciudad: "Queretaro"
+    edad: 20,
+    dirección:{
+        ciudad: "Queretaro",
+        pais: "Mexico"
+    }
 };
-// Extrae los valores de nombre, edad y ciudad usando destructuración
-const { nombre, edad, ciudad } = persona;
-// Muestra un mensaje con los valores extraídos
-console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
+// Aplica la destructuración aqui swgun el codigo anterior
+const { nombre, edad, dirección: { ciudad } } = persona;
+// Muestra el mensaje
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}`); // Me llamo Domingo Araujo, tengo 37 años y vivo en Queretaro
