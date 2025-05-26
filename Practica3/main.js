fbec3245a9d6b05ff1d3cf97c7c715ae14b3fb81
XGhostD <122043762@upq.edu.mx>
2025-05-25 19:28:33 -0600

    Ejercicio A

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+const {restar} = require('./util.js');
+
+console.log(restar(10, 5));   // Resultado: 5
+console.log(restar(3, 7));     // Resultado: -4
+console.log(restar(0, 0));     // Resultado: 0
+console.log(restar(-2, -8)); // Resultado: 6
+
