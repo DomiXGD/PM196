a086c5ed784a4dccf8ef37e34d0223aaec875ab4
XGhostD <122043762@upq.edu.mx>
2025-06-16 19:02:59 -0600

    Atributos de componentes

--- my-app2/App.js
+++ my-app2/App.js
@@ -1,24 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React,{useState} from 'react';
 
 
-    const Texto= (props)=>{
-      const {children}=props
+    const Texto= ({style})=>{
+      const [contenido, setContenido]=useState('Hola Mundo')
+      const actualizaTexto=()=>{setContenido('State Modificado')}
       return(
-        <Text> {children} </Text>
+        <Text style={[styles.text,style]} onPress={actualizaTexto}> {contenido} </Text>
       )
     }
+
+    // const Boton= ()=>{
+    //   const [cambioNuevo,setContenidoo]=useState('Presioname')
+    //   const actualizaBoton=()=>{setContenidoo('Ya me has presionado')}
+    //   return(
+    //     <Boton title={cambioNuevo} onPress={actualizaBoton}></Boton>
+    //   )
+    // }
+    
 
 export default function App() {
   return (
 
     <View style={styles.container}>
 
-      <Texto>"hola"</Texto>
-      <Texto>"Mundo"</Texto>
-      <Texto>React Native</Texto>
-
-      <Button title='Presioname'> </Button>
+      <Texto style={styles.green}></Texto>
+      <Texto style={styles.blue}></Texto>
+      <Texto style={styles.gray}></Texto>
+      {/* Quiero crear un boton que cuando lo pise se cambie el texto dentro del boton */}
+      {/*    <Button 
+      <Boton title='Pisame'> </Boton> */}
       <StatusBar style="auto" />
 
     </View>
@@ -30,6 +42,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'stretch',
   },
+
+  text:{
+    color: 'red',
+    fontSize: 27,
+    width: 200,
+    height: 50,
+  },
+
+  gray:{backgroundColor: 'gray', },
+  blue:{backgroundColor: 'blue', },
+  green:{backgroundColor: 'green',}
 });
